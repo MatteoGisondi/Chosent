c2a468c526fb11b0f13cdf079fa8dbb2cc561940
btakli <64867481+btakli@users.noreply.github.com>
2023-01-22 10:12:55 -0500

    Got collision and damage working for chosen

--- Logic/Chosen.cs
+++ Logic/Chosen.cs
@@ -11,16 +11,62 @@ namespace Chosent.Logic
 		private float _inputDelay = 0.1f * 8;
 		private int _movementSpeed = 64;
 
-		private (int,int) start = (0,0);
-		private (int,int) end = (7,7);
+		private (int, int) start = (0, 0);
+		private (int, int) end = (7, 7);
 
-		private List<(int,int)> movements;
+		private List<(int, int)> movements;
 		private int _moveIndex = 0;
 
 		private bool isPathGenerated = false;
 		private bool isAtEnd = false;
 
+		public int _hp = 4;
+		public int _str = 4;
+		public int _dex = 4;
+		public int _int = 4;
+
 		// Called when the node enters the scene tree for the first time.
+		public void CheckCollision()
+		{
+			// Get parent node of chosen (the level)
+			var level = (LevelToArray)this.GetParent();
+
+			// Get the chosen's position
+			var chosenPosition = this.Position;
+
+			// Check if any child of level other than the chosen is at the chosen's position
+			foreach (var child in level.GetChildren())
+			{
+				// Print the child's name
+				GD.Print(child);
+				if (child is Chosen)
+				{
+					continue;
+				}
+
+				if (child is Hazard)
+				{
+					if (((Hazard)child).GetPosition() == chosenPosition)
+					{
+						// If the child is a hazard, damage the chosen
+						Hazard childHazard = (Hazard)child;
+						this._hp -= childHazard._hp;
+						this._str -= childHazard._str;
+						this._dex -= childHazard._dex;
+						this._int -= childHazard._int;
+					}
+
+					// If any of the chosen's stats are 0, delete the chosen
+					if (_hp <= 0 || _str <= 0 || _dex <= 0 || _int <= 0)
+					{
+						this.QueueFree();
+					}
+				}
+			}
+
+
+		}
+
 		public override void _Ready()
 		{
 			_lastInput = DateTime.MinValue; // Set it to the minimum value so we can move immediately
@@ -55,6 +101,7 @@ namespace Chosent.Logic
 			{
 				_lastInput = DateTime.Now;
 				this.Move(this.movements[_moveIndex++]);
+				CheckCollision();
 				if (_moveIndex == this.movements.Count) isAtEnd = true;
 			}
 
@@ -73,6 +120,10 @@ namespace Chosent.Logic
 			}
 
 		}
+
+
+
+
 
 		/// <summary>
 		/// Handles the movement of the icon
@@ -109,7 +160,7 @@ namespace Chosent.Logic
 			}
 		}
 
-		public void Move((int,int) move)
+		public void Move((int, int) move)
 		{
 			// Move up, down, left, right on a grid with input from AStar!
 			// Move up
