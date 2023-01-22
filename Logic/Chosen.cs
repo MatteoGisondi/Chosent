b3e2bb662679c550b12045008120f19f3ae29332
MatteoGisondi <matteo.gisondi@gmail.com>
2023-01-22 09:47:28 -0500

    Add game state

--- Logic/Chosen.cs
+++ Logic/Chosen.cs
@@ -41,63 +41,26 @@ namespace Chosent.Logic
 		// Called every frame. 'delta' is the elapsed time since the previous frame.
 		public override void _PhysicsProcess(float delta)
 		{
-			// Check if we have waited long enough since the last input, if we have then move
-			// if (CheckInputDelay())
-			// {
-			// 	Movement();
-			// }
-			if (this.GetLevel().IsRendered() && !isPathGenerated)
+			var globals = GetNode<GlobalVariables>("/root/GlobalVariables");
+			if (globals.gameState != 0)
 			{
-				this.GetMovements();
-				isPathGenerated = true;
-			}
-			if (this.GetLevel().IsRendered() && isPathGenerated && CheckInputDelay() && !isAtEnd)
-			{
-				_lastInput = DateTime.Now;
-				this.Move(this.movements[_moveIndex++]);
-				if (_moveIndex == this.movements.Count) isAtEnd = true;
-			}
+				if (this.GetLevel().IsRendered() && !isPathGenerated)
+				{
+					this.GetMovements();
+					isPathGenerated = true;
+				}
+				if (this.GetLevel().IsRendered() && isPathGenerated && CheckInputDelay() && !isAtEnd)
+				{
+					_lastInput = DateTime.Now;
+					this.Move(this.movements[_moveIndex++]);
+					if (_moveIndex == this.movements.Count) isAtEnd = true;
+				}
 
-			if (this.GetLevel().IsRendered() && isPathGenerated && isAtEnd)
-			{
-				// TODO : win state
-				GetTree().Quit();
-			}
-
-		}
-
-		/// <summary>
-		/// Handles the movement of the icon
-		/// </summary>
-		public void Movement()
-		{
-			// Move up, down, left, right on a grid with arrow keys. Block diagomal movement
-			//Move up
-			if (Input.IsActionPressed("ui_up") && !Input.IsActionPressed("ui_left") && !Input.IsActionPressed("ui_right") && !Input.IsActionPressed("ui_down"))
-			{
-				Position += new Vector2(0, -_movementSpeed);
-				_lastInput = DateTime.Now;
-			}
-
-			//Move down
-			if (Input.IsActionPressed("ui_down") && !Input.IsActionPressed("ui_left") && !Input.IsActionPressed("ui_right") && !Input.IsActionPressed("ui_up"))
-			{
-				Position += new Vector2(0, _movementSpeed);
-				_lastInput = DateTime.Now;
-			}
-
-			//Move left
-			if (Input.IsActionPressed("ui_left") && !Input.IsActionPressed("ui_up") && !Input.IsActionPressed("ui_down") && !Input.IsActionPressed("ui_right"))
-			{
-				Position += new Vector2(-_movementSpeed, 0);
-				_lastInput = DateTime.Now;
-			}
-
-			//Move right
-			if (Input.IsActionPressed("ui_right") && !Input.IsActionPressed("ui_up") && !Input.IsActionPressed("ui_down") && !Input.IsActionPressed("ui_left"))
-			{
-				Position += new Vector2(_movementSpeed, 0);
-				_lastInput = DateTime.Now;
+				if (this.GetLevel().IsRendered() && isPathGenerated && isAtEnd)
+				{
+					// TODO : win state
+					GetTree().Quit();
+				}
 			}
 		}
 
