4be637faed984d840b4d3c9e83a4c5af9adf7c92
MatteoGisondi <matteo.gisondi@gmail.com>
2023-01-21 23:49:26 -0500

    Fix namespace

--- Logic/Node.cs
+++ Logic/Node.cs
@@ -1,28 +1,31 @@
-namespace ConsoleApp1.Logic;
-
-internal class Node
+namespace ConsoleApp1.Logic
 {
-    public int X { get; set; }
-    public int Y { get; set; }
-    public int G { get; set; }
-    public int H { get; set; }
-    public int F { get; set; }
-    public Node Parent { get; set; }
 
-    public Node(int x, int y, Node parent)
-    {
-        X = x;
-        Y = y;
-        Parent = parent;
-    }
 
-    public string ToString()
+    internal class Node
     {
-        return string.Format($"({X}, {Y}) [G={G}, H={H}, F={F}]");
-    }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int G { get; set; }
+        public int H { get; set; }
+        public int F { get; set; }
+        public Node Parent { get; set; }
 
-    public int CompareTo(Node other)
-    {
-        return F - other.F;
+        public Node(int x, int y, Node parent)
+        {
+            X = x;
+            Y = y;
+            Parent = parent;
+        }
+
+        public string ToString()
+        {
+            return string.Format($"({X}, {Y}) [G={G}, H={H}, F={F}]");
+        }
+
+        public int CompareTo(Node other)
+        {
+            return F - other.F;
+        }
     }
 }
