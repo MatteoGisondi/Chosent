d9aba12f437c8435cbf8c19f31a128003ea2e5a1
MatteoGisondi <matteo.gisondi@gmail.com>
2023-01-22 00:48:06 -0500

    Add Grid as TileMap to MainLevel scheme

--- Logic/MainLevel.cs
+++ Logic/MainLevel.cs
@@ -1,19 +1,30 @@
 using Godot;
 using System;
 
-public class MainLevel : Node2D
-{
+public class Grid : Node2D {
     int[,] grid;
-    TileMap tileMap;
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
+    TileMap tilemap;
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void _Ready() {
+        grid = new int[10,10];
+
+        tilemap = new TileMap();
+        AddChild(tilemap);
+
+        // Set the tilemap size to match the grid size
+        tilemap.CellSize = new Vector2(32, 32);
+        tilemap.SetSize(grid.GetLength(0), grid.GetLength(1));
+
+        var tile_set = new TileSet();
+        tile_set.CreateTileSet(new Vector2(32, 32));
+        tilemap.TileSet = tile_set;
+
+        for (int i = 0; i < grid.GetLength(0); i++) {
+            for (int j = 0; j < grid.GetLength(1); j++) {
+                int tile_id = grid[i, j];
+                tilemap.SetCell(i, j, tile_id);
+            }
+        }
+    }
 }
+
